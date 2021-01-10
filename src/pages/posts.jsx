cf520f4e0cd51ba487cda6602a1251d98a394f82
Christian Lisangola <jochri3@gmail.com>
2021-01-10 17:33:33 +0100

    Exploring the query object

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -2,12 +2,15 @@ import PostsList from "../components/posts/posts.list";
 import TextInput from "../components/text-input/text-input";
 import Count from "../components/count/count";
 import Loader from "../components/loader/loader";
+import { useQuery } from "react-query";
 
 export default function Posts() {
     const data=[
         {id:1,title:"blablabla",content:"Bliblibli"},
         {id:2,title:"tototo",content:"tititi"}
     ]
+  const queryInfos=useQuery();
+  console.log(queryInfos);
   return (
     <div className="">
       <h1 className="heading-primary">All posts</h1>
